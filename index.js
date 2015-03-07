9816485edf27a1c2571e5a7b4cb083088bda8d58
Will McCambley <will@codehs.com>
2015-03-07 15:13:49 -0800

    refactor base MEAN STACK

--- index.js
+++ index.js
@@ -6,26 +6,56 @@ var http = require("http");
 var express = require("express");
 var mongoose = require("mongoose");
 var app = express();
-var url = require("url");
 
-mongoose.connect('mongodb://localhost/firstCup');
-
-app.use(express.static(__dirname + '/public'));
-
-//server
-// function requestListener(request,response){
-// 	console.log('bro' + port)
-// } 
+//Server
 var server = http.createServer(app);
 
 app.get('/bro', function(request, response){
 	console.log(request.url);
 })
 
-
-
-// var server = require("http").createServer(app);
-
 server.listen(port, function(){
 	console.log('Listening on port ' + port);
 });
+
+//Serving Static Files
+app.use(express.static(__dirname + '/public'));
+
+//Mongo Intialization
+
+mongoose.connect('mongodb://localhost/firstCup');
+var db = mongoose.connection;
+db.on('error', function (err) {
+	console.log('connection error', err);
+});
+db.once('open', function () {
+	console.log('connected.');
+});
+
+//Define Schema
+var Schema = mongoose.Schema;
+var userSchema = new Schema({
+	name : String,
+});
+
+// Schema to DB Model
+var User = mongoose.model('User', userSchema);
+
+//Test DB Object
+var squid = new User({
+name : 'Squad',
+});
+ 
+ //Save DB Object
+squid.save(function (err, data) {
+if (err) console.log(err);
+else console.log('Saved : ', data );
+});
+
+
+
+
+
+
+
+
