b2394a80b3528509685d838e7ad53bbc2a00e030
Liam Schauerman <ljschauerman@gmail.com>
2015-04-12 21:29:30 -0700

    navbar login only shows if user is logged out

--- public/angular/controllers/navController.js
+++ public/angular/controllers/navController.js
@@ -3,13 +3,16 @@ angular.module('firstCup.nav', []).controller("NavController", function($scope, 
 	if (!$window.signedIn){
 		$window.signedIn = false;
 	};
-
+	// $scope.notLoggedIn = true
 	User.getUser().then(function(resp){
 		console.log(resp);
 		if (resp.data.local){
 			$scope.user = resp.data.local;
 			$window.signedIn = true;
-		};
+			$scope.notLoggedIn = false;
+		} else {
+			$scope.notLoggedIn = true;
+		}
 	});
 	$scope.submit = function(submission){
 		Links.postLink(submission);
